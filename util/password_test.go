baded77f11b1ed9389b8e3d43d22010bb7a237c2
Kurush Qosimi <kurushqosimi@gmail.com>
2025-01-25 00:10:36 +0500

    create user route and tests related to it added

---
+++ util/password_test.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword1, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword1)
+
+	err = CheckPassword(password, hashedPassword1)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashedPassword1)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+	require.NotEqual(t, hashedPassword1, hashedPassword2)
+}
+
+func TestWrongPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
+
+func TestErrorPassword(t *testing.T) {
+	password := RandomString(73)
+	_, err := HashPassword(password)
+	require.EqualError(t, err, fmt.Errorf("failed to hash password: %w", bcrypt.ErrPasswordTooLong).Error())
+}
